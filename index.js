dc7e5ea83b33159e8c3f6d3aa9554b3ea137a8ef
Brandon Blankenstein <blankensteinpercussion@gmail.com>
2020-12-12 13:45:13 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+var fullName = 'Johnathan Smithson';
+var dateOfBirth = 'June 1, 1984';
+var age = 36;
+var isOrganDonor = true;
+var streetAddress = '123 Place Drive';
+var city = 'Greenton';
+var state = 'MA';
+var zipPostalCode = 12345;
+var idNumber = 987654321;
+
+var bio = `Hello, my name is ${fullName} and I was born on ${dateOfBirth}. I am ${age} years old and I live at ${streetAddress} in ${city}, ${state} ${zipPostalCode}. My ID number is ${idNumber}.`
+
+console.log(bio);
---
+++ notes.js
@@ -0,0 +1,13 @@
+// JavaScript Strings
+
+var make = 'Toyota';
+var model = 'Prius';
+var year = 2017;
+
+// String Concatenation
+
+var myRide = make + ' ' + model;
+
+var myBraggingRight = 'I drive a ' + year + ' ' + myRide;
+
+console.log(myRide.length);
